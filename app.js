b4d8c1cf7ddbb4c7137df778144c08a6fae9c0f3
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-11-04 14:54:33 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var promoRouter = require('./routes/promorouter');
 var leaderRouter = require('./routes/leaderrouter');
 
 
-const Mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-const connect = Mongoose.connect(url);
+const connect = mongoose.connect(url);
 
 connect.then((db) => {
   console.log('connected correctly to server');
@@ -33,6 +33,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if(!authHeader){
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+  var username = auth[0]; var password = auth[1];
+
+  if(username === 'admin' && password === 'password') {
+    next();
+  }
+  else {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
