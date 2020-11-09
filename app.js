5cfd328b700f3cc01694367605067aa605f49fb1
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-11-09 15:58:54 +0530

    File Upload

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishrouter');
 var promoRouter = require('./routes/promorouter');
 var leaderRouter = require('./routes/leaderrouter');
-
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -59,6 +59,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
