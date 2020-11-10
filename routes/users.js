2e2981a96f3cbac10bc1126491968e1179c81485
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-11-10 12:20:02 +0530

    Cross Origin Resource Sharing

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,10 @@ var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 var router = express.Router();
+const cors = require('./cors');
 router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/',authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+router.get('/',cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
   User.find({})
         .then((err, users) => {
             if (err) {
@@ -19,7 +20,7 @@ router.get('/',authenticate.verifyUser, authenticate.verifyAdmin, function(req, 
         .catch((err) => next(err));
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup',cors.corsWithOptions,  (req, res, next) => {
   User.register(new User({username: req.body.username}), 
   req.body.password, (err, user) =>{
     if(err) {
@@ -51,14 +52,14 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res, next) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res, next) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are Successfully Logged in.'});
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
