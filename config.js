e2eb2c98f5ac0f1c039744212eac4afd9dba9dcb
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-11-10 17:53:08 +0530

    OAuth 2.0 Authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '695434151368806',
+        clientSecret: 'f792c883f3cf83ae1e02fbdde14cb97b'
+    }
 }
