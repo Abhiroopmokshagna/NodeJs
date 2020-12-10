4f98f1ca7084cc5e8f1de6f210c2a603235749ec
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2020-12-10 11:59:54 +0530

    Hackathon Backend

--- routes/users.js
+++ routes/users.js
@@ -89,6 +89,25 @@ router.get('/logout', cors.corsWithOptions, (req, res) => {
   }
 });
 
+router.get('/checkJWTtoken', cors.corsWithOptions, (req, res) => {
+  passport.authenticate('jwt', {session: false}, (err, user, info) => {
+    if (err)
+      return next(err);
+    
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT invalid!', success: false, err: info});
+    }
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      return res.json({status: 'JWT valid!', success: true, user: user});
+
+    }
+  }) (req, res);
+});
+
 router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
   if (req.user) {
     var token = authenticate.getToken({_id: req.user._id});
